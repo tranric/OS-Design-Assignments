50bec1d271261bdc4debc6d1613a2aea26f70524
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-10 14:05:37 -0400

    added the armstrong process
    forgot to add the comments until now

--- Assignment 1 Question 1.c
+++ Assignment 1 Question 1.c
@@ -33,3 +33,26 @@ int ArmstrongNumber(int arg1, int arg2, int arg3){
 	
 	
 }
+//used to prevent messing with the for loop in armstrong number method
+//process is the number for the for loop
+//digit is the number of digits to look for from the arg1
+//pow how it works i forgot and had to relook = pow(base, power)
+//note pow uses double must convert to int afterwords
+//output is in double for the math.pow method to be more easily
+int processArmstrong(int process, int digit){
+	double output=0;
+	
+	while(process){
+		//formula to process each digit of the current number for the armstrong formula
+		//and processes to the next digit in the number
+		double processnumber = pow(process % 10, digit);
+		output += digit;
+		process /= 10;
+	}
+	//returns the result at the end, converts back to int from the math.pow's double
+	return (int) output;
+}
+
+
+
+
