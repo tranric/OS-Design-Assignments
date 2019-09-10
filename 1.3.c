a9c521109cd218040eed23d3a40345dc8b47a054
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-10 18:21:46 -0400

    renaming files
    renaming files to proper naming conventions based on pdf

---
+++ 1.3.c
@@ -0,0 +1,13 @@
+/*
+Question 3 - 
+(2 points) Using pointers and avoiding unnecessary local variables, write a function, rmchr
+that takes a string and a character as command line arguments. The rmchr function should
+remove all occurrences of the character from the string. The rmchr function should not leave
+spaces characters in the string where characters have been removed. The resulting string
+should be returned using an appropriate data type and printed to the command line.
+*/
+
+//Group Members: Daniel Saccheda and Richard Tran
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
