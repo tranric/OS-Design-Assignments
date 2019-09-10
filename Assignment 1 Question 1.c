2ad82bc225c63fa60ce156bc411167af7112f824
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-10 13:09:46 -0400

    Basic structure
    added basic structures and TODOs

--- Assignment 1 Question 1.c
+++ Assignment 1 Question 1.c
@@ -1,7 +1,17 @@
 //Create by Richard Tran
-//Group Members: Daniel and Richard
+//Group Members: Daniel Saccheda and Richard Tran
 #include <stdio>
 #include <stdlib>
 #include <math.h>
 
-//test commit on windows Desktop
+//initalized variables for holding the arguments for the program
+//a for argument 1, b for arg 2, etc.
+int a=0, b=0, c=0;
+
+//main method
+int main(int argc, char *argv[]){
+	//TO DO: get numbers, get each character for each number 
+	//TO DO: calculations
+	
+	
+}
