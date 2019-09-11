29ea5e7e394641b5e3e55afea41f362c0cae0d30
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-11 13:17:22 -0400

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -24,11 +24,9 @@ char* rmchr(char* string, char* character) {
 		//jumppoint:
 	 for (int i = 0; i < strlen(string); i++) {
 		 //printf("for loop: i is location: %d\n letter of string is:%c \n", i, string[i]);
-		 char stringlocation = string[i];
-		 int a=atoi(character[0]);
-		 int b=atoi(stringlocation);
+		 
 		 //if character is found remove character at location
-		  if (a == b) {
+		  if (character[0] == string[i]) {
 			  //https://www.tutorialspoint.com/c_standard_library/c_function_memmove.htm
 			  //how memmove works above (note for self ~richard)
 			  
