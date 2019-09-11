bb2f81a439f599b647176c15b005f9abdc28d476
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-11 13:03:56 -0400

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -18,11 +18,11 @@ should be returned using an appropriate data type and printed to the command lin
 //checks each character of the string for the second (3rd) argument for the same matching character.
 //and removes the matching character and returns the new string output.
 char* rmchr(char* string, char* character) {
-	int stringlength = strlen(string);
+	//int stringlength = strlen(string);
 	//gets the length of the string as the maximum of the for loop to not break the program
 	//while(string != '\0'){
-	 for (int i = 0; i < stringlength; i++) {
-		 printf("for loop: i is location: %d\n letter of string is:%c \n", i, string[i]);
+	 for (int i = 0; i < strlen(string); i++) {
+		 //printf("for loop: i is location: %d\n letter of string is:%c \n", i, string[i]);
 		 
 		 //if character is found remove character at location
 		  if (character[0] == string[i]) {
@@ -62,7 +62,7 @@ int main(int argc, char** argv){
 
     char *str = rmchr(argv[1], argv[2]);
 	//added new line because it prints on same line as command is run.
-    printf("\n%s", str);
+    printf("\n%s\n", str);
 
     return 0;
 }
