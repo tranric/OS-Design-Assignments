1306f542d5c5f702e99aa9eeb6eac8558e6462db
saccheda <saccheda@sheridancollege.ca>
2019-09-11 13:52:15 -0400

    changes addid for loop to work in case of double letters

--- 1.3.c
+++ 1.3.c
@@ -17,54 +17,28 @@ should be returned using an appropriate data type and printed to the command lin
 //remove character method
 //checks each character of the string for the second (3rd) argument for the same matching character.
 //and removes the matching character and returns the new string output.
-char* rmchr(char* string, char* character) {
-	//int stringlength = strlen(string);
-	//gets the length of the string as the maximum of the for loop to not break the program
-	//while(string != '\0'){
-	 for (int i = 0; i < strlen(string); i++) {
-		 //printf("for loop: i is location: %d\n letter of string is:%c \n", i, string[i]);
-		 
-		 //if character is found remove character at location
-		  if (character[0] == string[i]) {
-			  //https://www.tutorialspoint.com/c_standard_library/c_function_memmove.htm
-			  //how memmove works above (note for self ~richard)
-			  
-			  //takes source string and shift the string from the part it found the 
-			  //character over one. replacing all of the old letters with the next 
-			  //character over, repeats until end of character.
-			  memmove(&string[i], &string[i + 1], strlen(string));
-			  //due to the current i now a new letter it needs to be rechecked in the for loop
-			  //thus moving one back again to recheck before adding another ++ to i.
-			  //since it was shifted over to the left once.
-				//printf("i is location: %d\n letter of string is:%c \n", i, string[i]);
-			    
-				/*
-				for(int x=0; x <stringlength; x++){
-						string[i]=string[i+1];
-				}
-				*/
-				i--;
-				//string[i-1];
-				//stringlength--;
-				//printf("i is location: %d\n letter of string is:%c \n", i, string[i]);
-				
-		  }
+char *str;
+
+char* rmchr(char* string, char* character) 
+{
 	
-	}
-	//}
-	 //original string is overwritten in the for loop returning the modified string (will need to change for 
-	 //question 4)
+	 for (int i = 0; i < strlen(string); i++) {
+		  while(character[0] == string[i]) {
+			  for(int j = i; j < strlen(string); j++){
+			  string[j]= string[j + 1];
+			  }				
+	 }
+	 }
 	 return string;	
 }
-
 //main just calls the method and returns the new string
 int main(int argc, char** argv){
 
-    char *str = rmchr(argv[1], argv[2]);
+	str = argv[1];
+	char *c = (char*) argv[2];
+    char *str2 = rmchr(str, c);
 	//added new line because it prints on same line as command is run.
-    printf("\n%s\n", str);
-
+    printf("\n%s\n", str2);
+    
     return 0;
 }
-
-
