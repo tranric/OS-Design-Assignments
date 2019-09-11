f7f549bf2aa8b70748461cf8ff997e4f4ab944d8
leiroux <55151012+tranric@users.noreply.github.com>
2019-09-11 13:47:15 -0400

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -8,7 +8,23 @@
 #include <stdlib.h>
 #include <math.h>
 
+//checks if its coprime by running through until it equals 0
+int isprime(int a, int b){
+	if (a==0){
+		return b;
+	}
+	return isCoprime(b%a,a);
+}
 
+int phi(int a){
+	//starts at 2 since 1 is always included
+	int number =1; 
+	for (int i = 2; i < a; i++){		
+		if (isprime(i,a)==1)
+			number++;
+	}
+	return number;		
+}
 
 
 
