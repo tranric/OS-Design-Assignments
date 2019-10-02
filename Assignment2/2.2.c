e4bb07d6f9942e993a8d16e4be0cde34e674c2dd
leiroux <55151012+tranric@users.noreply.github.com>
2019-10-02 14:42:53 -0400

    comments for 2.2

--- Assignment2/2.2.c
+++ Assignment2/2.2.c
@@ -24,34 +24,34 @@ int main (int argc, char *argv[]) {
     char* dst = argv[2];
 	
 	 switch(fork()) {
-
+	//runs if the pipes and forks are not working properly
       case -1:
 		printf("Error forking child process. %s\n", strerror(errno));
 		exit(1);
 
       case 0: 
-	  
+	  //closes pipes for next fork to make sure it does not runthrough again.
 		close(pipeFds[1]); 
-	  
+	  //sets the size and reads the pipe and saves it to the buffer of the child.
         ssize_t num_bytes_child = read(pipeFds[0], buffer2, sizeof(buffer2));
-		
+		//closes the second pipe to ensure nothing else is modified
         close(pipeFds[0]);  
-		
+		//sets up the stream for writing using the buffer
         int targetDesc = open(dst, O_CREAT | O_WRONLY);
-		
+		//writes data to the destination with the size, and data from buffer. to not overwrite with null values.
         write(targetDesc, buffer2, num_bytes_child);
 	  
 		
 	  default:
-	    
+	    //ensures it is closed from case 0 
 		close(pipeFds[0]);
-		
+		//sets up the stream in read only to not be edited.
         int fileInDesc = open(src, O_RDONLY); 
-		
+		//sets the size of read from buffer and saves it to num of bytes variable
         ssize_t num_bytes = read(fileInDesc, buffer, sizeof(buffer));
-		
+		//writes to the second pipe/child process using the buffer
         write(pipeFds[1], buffer, num_bytes);
-		
+		//closes the second pipe to ensure all pipes are closed.
         close(pipeFds[1]);
 	  
 
