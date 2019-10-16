008f8753c810f3daeda85a75cfcac7c1dc3c4480
leiroux <55151012+tranric@users.noreply.github.com>
2019-10-16 12:10:40 -0400

    added thread import

--- Assignment3/stats.c
+++ Assignment3/stats.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 
 void *findAvg(void *arg){
@@ -23,7 +24,7 @@ void *findMin(void *arg){
 
 int main(int argc, char *argv[])
 {
-	
+
 	
 }
 
