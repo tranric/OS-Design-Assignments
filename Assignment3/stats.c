d192a3e5947dae1f4c1e609ba1770fba2562c835
leiroux <55151012+tranric@users.noreply.github.com>
2019-10-16 12:13:40 -0400

    threads and getting args

--- Assignment3/stats.c
+++ Assignment3/stats.c
@@ -24,7 +24,16 @@ void *findMin(void *arg){
 
 int main(int argc, char *argv[])
 {
-
+	//3 threads for each function
+	pthread_t thread1;
+    pthread_t thread2;
+    pthread_t thread3;
+	
+	//gets the arguments and saves it into a variable -1 due to
+	//calling the file itself being included in arguments to be removed.
+	int *numofargs = (int*)malloc((argc-1)*sizeof(int));
+	
+	
 	
 }
 
