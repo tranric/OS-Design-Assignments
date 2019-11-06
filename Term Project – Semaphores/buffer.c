a7477e3411a86d9c763c55a17b37d398e2a42219
leiroux <55151012+tranric@users.noreply.github.com>
2019-11-06 13:04:40 -0500

    reading visibility
    spaced properly and added random sleep function properly and changed error text.

--- Term Project – Semaphores/buffer.c
+++ Term Project – Semaphores/buffer.c
@@ -6,50 +6,57 @@
 buffer_item buffer[BUFFER_SIZE];
 
 void *producer(void *param) {
-buffer item item;
-while (true) {
-/* sleep for a random period of time */
-sleep(...);
-/* generate a random number */
-item = rand();
-if (insert item(item))
-fprintf("report error condition");
-else
-printf("producer produced %d\n",item);
-}
+	
+	buffer_item item;
+	
+	while (true) {
+		/* sleep for a random period of time */
+		sleep(rand() % 3 + 1);
+		/* generate a random number */
+		item = rand();
+		if (insert item(item)){
+			fprintf("error cannot add item");
+		}
+		else{
+			printf("producer produced %d\n",item);
+		}
+	}
 }
 void *consumer(void *param) {
-buffer item item;
-while (true) {
-/* sleep for a random period of time */
-sleep(...);
-if (remove item(&item))
-fprintf("report error condition");
-else
-printf("consumer consumed %d\n",item);
-}
+	buffer_item item;
+	
+	while (true) {
+		/* sleep for a random period of time */
+		sleep(rand() % 3 + 1);
+		if (remove item(&item)){
+			fprintf("error cannot remove item");
+		}
+		else{
+			printf("consumer consumed %d\n",item);
+		}
+	}
 }
 
 int insert item(buffer item item) {
-/* insert item into buffer
-return 0 if successful, otherwise
-return -1 indicating an error condition */
+	/* insert item into buffer
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
 }
 int remove item(buffer item *item) {
-/* remove an object from buffer
-placing it in item
-return 0 if successful, otherwise
-return -1 indicating an error condition */
+	/* remove an object from buffer
+	placing it in item
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
 }
 
 int main(int argc, char *argv[]) {
 	/* 1. Get command line arguments argv[1],argv[2],argv[3]
-*/
-/* 2. Initialize buffer */
-/* 3. Create producer thread(s) */
-/* 4. Create consumer thread(s) */
-/* 5. Sleep */
-/* 6. Exit */
+	*/
+	/* 2. Initialize buffer */
+	/* 3. Create producer thread(s) */
+	/* 4. Create consumer thread(s) */
+	/* 5. Sleep */
+	/* 6. Exit */
 	
 }
 
